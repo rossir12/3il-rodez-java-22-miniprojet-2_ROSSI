4afb6d1e63ab02267392bf463e3b7381b034143c
RRossi81 <151721460+rossir12@users.noreply.github.com>
2024-03-28 19:29:35 +0100

    Javadoc de la classe Noeud

--- src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
+++ src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
@@ -3,25 +3,44 @@ package fr.ecole3il.rodez2023.carte.elements;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Représente un noeud dans un graphe.
+ * @param <E> le type de valeur stockée dans le noeud
+ */
 public class Noeud<E> {
     private E valeur;
     private List<Noeud<E>> voisins;
 
+    /**
+     * Initialise un nouveau noeud avec une valeur donnée.
+     * @param valeur la valeur du noeud
+     */
     public Noeud(E valeur) {
         this.valeur = valeur;
         this.voisins = new ArrayList<>();
     }
 
+    /**
+     * Récupère la valeur du noeud.
+     * @return la valeur du noeud
+     */
     public E getValeur() {
         return valeur;
     }
 
+    /**
+     * Récupère la liste des voisins du noeud.
+     * @return la liste des voisins du noeud
+     */
     public List<Noeud<E>> getVoisins() {
         return voisins;
     }
 
+    /**
+     * Ajoute un voisin au noeud.
+     * @param voisin le voisin à ajouter
+     */
     public void ajouterVoisin(Noeud<E> voisin) {
         this.voisins.add(voisin);
     }
 }
-
