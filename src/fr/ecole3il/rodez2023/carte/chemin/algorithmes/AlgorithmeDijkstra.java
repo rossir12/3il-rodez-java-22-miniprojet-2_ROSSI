bb18d822f777c6d955ae13c2de381c9202d0759d
RRossi81 <151721460+rossir12@users.noreply.github.com>
2024-03-28 19:30:00 +0100

    Javadoc de la classe AlgorithmeDijkstra

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeDijkstra.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeDijkstra.java
@@ -4,8 +4,18 @@ import fr.ecole3il.rodez2023.carte.elements.Graphe;
 import fr.ecole3il.rodez2023.carte.elements.Noeud;
 import java.util.*;
 
+/**
+ * Implémente l'algorithme de Dijkstra pour trouver le chemin le plus court entre deux noeuds dans un graphe pondéré.
+ * @param <E> le type d'élément stocké dans chaque noeud du graphe
+ */
 public class AlgorithmeDijkstra<E> implements AlgorithmeChemin<E> {
-
+    /**
+     * Trouve le chemin le plus court entre un noeud de départ et un noeud d'arrivée dans un graphe pondéré en utilisant l'algorithme de Dijkstra.
+     * @param graphe le graphe dans lequel rechercher le chemin
+     * @param depart le noeud de départ du chemin
+     * @param arrivee le noeud d'arrivée du chemin
+     * @return la liste des noeuds formant le chemin le plus court, du noeud de départ au noeud d'arrivée
+     */
     @Override
     public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
         // Initialisation des structures de données
