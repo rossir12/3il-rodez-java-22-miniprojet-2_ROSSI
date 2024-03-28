108180b5ad14033f8336e2756a44e5210f388101
RRossi81 <151721460+rossir12@users.noreply.github.com>
2024-03-28 19:32:29 +0100

    ExempleCLI Correction & Javadoc
    Utilisation du bon algorithme pour trouver un chemin entre deux points sur la carte

--- src/fr/ecole3il/rodez2023/carte/application/ExempleCLI.java
+++ src/fr/ecole3il/rodez2023/carte/application/ExempleCLI.java
@@ -9,21 +9,28 @@ import fr.ecole3il.rodez2023.carte.elements.Chemin;
 import fr.ecole3il.rodez2023.carte.manipulateurs.GenerateurCarte;
 
 /**
- * @author p.roquart
- * genre on peut tester le code
- * c'est agaçant tous ces tests
- * 
- * de toute façon quand ça compile c'est bon
+ * La classe ExempleCLI est un exemple simple d'utilisation des algorithmes de chemin avec une carte.
+ * Elle génère une carte, trouve un chemin entre deux points en utilisant un algorithme de chemin spécifié,
+ * puis affiche le chemin trouvé.
  */
 public class ExempleCLI {
-
+/**
+     * Méthode principale pour exécuter l'exemple.
+     *
+     * @param args les arguments de la ligne de commande (non utilisés dans cet exemple)
+     */
 	public static void main(String[] args) {
+		// Génération d'une carte de test
 		GenerateurCarte generateur = new GenerateurCarte();
 		Carte test = generateur.genererCarte(100,100);
+		// Utilisation de l'algorithme de Dijkstra pour trouver un chemin entre deux points sur la carte
 		AlgorithmeChemin algoChemin = new AlgorithmeDijkstra();
-		Chemin chemin = algoChemin.trouverChemin(test, 0, 0, 50, 50);
+		Chemin chemin = AdaptateurAlgorithme.trouverChemin(algoChemin, test, 0, 0, 50, 50);
+		// Affichage du chemin trouvé
 		chemin.afficherChemin();
-		chemin = algoChemin.trouverChemin(test, 0, 0, 50, 50);
+		// Utilisation de l'algorithme de Dijkstra pour trouver un autre chemin entre deux points sur la carte
+		chemin = AdaptateurAlgorithme.trouverChemin(algoChemin, test, 0, 0, 50, 50);
+		// Affichage du deuxième chemin trouvé
 		chemin.afficherChemin();
 	}
 	
