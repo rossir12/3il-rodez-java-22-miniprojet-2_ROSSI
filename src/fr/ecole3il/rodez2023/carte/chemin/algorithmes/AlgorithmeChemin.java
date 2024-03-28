a9d8daadaba042d174180f31c7caaaef95f2a0ba
RRossi81 <151721460+rossir12@users.noreply.github.com>
2024-03-28 19:30:16 +0100

    Javadoc de la classe AlgorithmeChemin

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeChemin.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeChemin.java
@@ -4,8 +4,18 @@ import fr.ecole3il.rodez2023.carte.elements.Graphe;
 import fr.ecole3il.rodez2023.carte.elements.Noeud;
 import java.util.List;
 
+/**
+ * Interface représentant un algorithme permettant de trouver un chemin dans un graphe.
+ * @param <E> le type d'élément stocké dans chaque noeud du graphe
+ */
 public interface AlgorithmeChemin<E> {
+
+    /**
+     * Trouve un chemin dans le graphe entre un noeud de départ et un noeud d'arrivée.
+     * @param graphe le graphe dans lequel rechercher le chemin
+     * @param depart le noeud de départ du chemin
+     * @param arrivee le noeud d'arrivée du chemin
+     * @return la liste des noeuds formant le chemin trouvé
+     */
     List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee);
 }
-
-
